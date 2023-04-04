2483b84a460ed188d85e22b559af220d2527c2f8
Allan Faé <allan_fae@hotmail.com>
2023-04-04 09:56:55 -0300

    Changed Link, Finished.

--- javaStickers/src/App.java
+++ javaStickers/src/App.java
@@ -3,15 +3,28 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_v0a519u2";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         var client = HttpClient.newHttpClient();
         URI adress = URI.create(url);
         var request = HttpRequest.newBuilder(adress).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+       // System.out.println(body);
+
+        var parser  = new JsonParser();
+
+        List<Map<String, String>> moovieList = parser.parse(body);
+
+        for (Map<String,String> filme : moovieList) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));           
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
